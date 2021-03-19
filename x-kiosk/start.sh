df84469bde179fa586267181a39fa88d681c3a87
Michael Scott <mike@foundries.io>
2021-03-19 14:11:18 -0700

    x-kiosk: cleanup start
    - Turn off shell debug
    - Wait for display
    - Add basic logging
    
    Signed-off-by: Michael Scott <mike@foundries.io>

--- x-kiosk/start.sh
+++ x-kiosk/start.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
-set -x
+
+# wait for display
+if [ ! -e /sys/class/graphics/fb0 ]; then
+    echo "Display not detected.  Will wait for it to show up ..."
+fi
+while [ ! -e /sys/class/graphics/fb0 ]; do
+    sleep 1
+done
 
 # Execute all the rest
+echo "Starting Chromium: $@"
 /usr/bin/chromium --no-sandbox --start-maximized --no-first-run --disable --disable-translate --disable-infobars --disable-suggestions-service --disable-save-password-bubble --incognito --autoplay-policy=no-user-gesture-required "$@" -- -nocursor -s 0 -dpms
