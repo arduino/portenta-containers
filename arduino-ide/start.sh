450c8d5ba19522eefdc29f200bba9b33c3062c3d
Michael Scott <mike@foundries.io>
2021-03-19 14:10:39 -0700

    arduino-ide: cleanup start
    - Turn off shell debug prints
    - Wait for display to show up before starting
    - Add some basic logging info
    
    Signed-off-by: Michael Scott <mike@foundries.io>

--- arduino-ide/start.sh
+++ arduino-ide/start.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-set -x
 
 # setup preferences.txt
 mkdir -p ${ARDUINO_HOME}/.arduino15
@@ -7,11 +6,21 @@ if [ ! -f /home/fio/.arduino15/preferences.txt ]; then
     cp /arduino/lib/preferences.txt ${ARDUINO_HOME}/.arduino15/
 fi
 
+# wait for display if needed
+if [ ! -e /sys/class/graphics/fb0 ]; then
+    echo "Display not detected.  Will wait for it to show up ..."
+fi
+while [ ! -e /sys/class/graphics/fb0 ]; do
+    sleep 1
+done
+
 VIRT_SIZE=$(cat /sys/class/graphics/fb0/virtual_size)
 VIRT_WIDTH=$(echo "${VIRT_SIZE}" | cut -d "," -f 1)
 VIRT_HEIGHT=$(echo "${VIRT_SIZE}" | cut -d "," -f 2)
 sed -i -e "s/editor.window.width.default.*/editor.window.width.default=${VIRT_WIDTH}/g" ${ARDUINO_HOME}/.arduino15/preferences.txt
 sed -i -e "s/editor.window.height.default.*/editor.window.height.default=${VIRT_HEIGHT}/g" ${ARDUINO_HOME}/.arduino15/preferences.txt
+echo "Display size: ${VIRT_WIDTH}x${VIRT_HEIGHT}."
 
 # Execute all the rest
+echo "Starting IDE."
 /arduino/arduino "$@"
