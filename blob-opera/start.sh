ae14fba15a6d9cb5dc6ff03e8e2c82663bb9bf5e
Massimo Pennazio <maxipenna@libero.it>
2022-01-14 12:22:48 +0100

    Added blob opera docker-compose app

---
+++ blob-opera/start.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# wait for display
+if [ ! -e /sys/class/graphics/fb0 ]; then
+    echo "Display not detected.  Will wait for it to show up ..."
+fi
+while [ ! -e /sys/class/graphics/fb0 ]; do
+    sleep 1
+done
+
+# use /tmp for config and cache data in read-only mode
+mkdir -p ${XDG_CACHE_HOME} ${XDG_CONFIG_HOME}
+
+# Execute all the rest
+echo "Starting Chromium: $@"
+/usr/bin/chromium \
+  --no-sandbox --start-maximized --no-first-run --disable --disable-translate \
+  --disable-infobars --disable-suggestions-service --disable-save-password-bubble \
+  --incognito --autoplay-policy=no-user-gesture-required --temp-profile \
+  --disable-dev-shm-usage --kiosk "$@" -- -nocursor -s 0 -dpms
