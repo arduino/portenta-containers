88ee447bc3057dcd0d39cd19970976bcc103aca9
Massimo Pennazio <maxipenna@libero.it>
2022-03-08 15:34:51 +0100

    Fixed problems but implementation to be reviewed

--- go-webapp-python-devel-arduino/go-webapp-arduino/entrypoint.sh
+++ go-webapp-python-devel-arduino/go-webapp-arduino/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 echo "Setting correct permissions to ssh keys"
-chmod -R 700 /root/.ssh
+mkdir -p /root/.ssh
+cp /tmp/keys/id_rsa /tmp/keys/id_rsa.pub /root/.ssh
+chmod 700 /root/.ssh
 chmod 644 /root/.ssh/id_rsa.pub
 chmod 600 /root/.ssh/id_rsa
 
--- go-webapp-python-devel-arduino/python-devel-arduino/entrypoint.sh
+++ go-webapp-python-devel-arduino/python-devel-arduino/entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 echo "Setting correct permissions to ssh keys"
-chmod -R 700 /root/.ssh
+mkdir -p /root/.ssh
+cat /tmp/keys/id_rsa.pub > /root/.ssh/authorized_keys
+chmod 700 /root/.ssh
 chmod 600 /root/.ssh/authorized_keys
 
 echo "Starting dropbear ssh server"
-dropbear -F -E -R -w -s -j -k
+dropbear -F -E -R -s -j -k
