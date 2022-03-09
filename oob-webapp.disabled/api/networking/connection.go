d2e8ce362e0bf2f5bd8979345d3287d3f7c773b3
Luca Barbetti <lbarbetti@live.it>
2022-03-09 17:13:44 +0100

    Merge commit '3d97b0fc9c754484121dc9a085788ccf0d88af37'

--- oob-webapp.disabled/api/networking/connection.go
+++ oob-webapp.disabled/api/networking/connection.go
@@ -4,11 +4,12 @@ import (
 	"encoding/csv"
 	"fmt"
 	"io"
-	"log"
 	"regexp"
 	"strings"
 
 	utils "x8-oob/utils"
+
+	log "github.com/inconshreveable/log15"
 )
 
 func GetConnection(grep string) (*Connection, error) {
@@ -35,21 +36,20 @@ func GetConnection(grep string) (*Connection, error) {
 			break
 		}
 		if err != nil {
-			log.Fatal(err)
+			return nil, fmt.Errorf("parsing nmcli output: %w", err)
 		}
 
 		net = record[0]
 		nic = record[3]
 	}
 
-	fmt.Printf("ip -4 -br address | grep %s\n", nic)
-	out, err = utils.ExecSh(fmt.Sprintf("ip -o -f inet addr | grep %s", nic))
+	out, err = utils.ExecSh(fmt.Sprintf("nmcli device show %s", nic))
 	if err != nil {
 		return nil, fmt.Errorf("reading network ip for NIC %s: %w", nic, err)
 	}
-	fmt.Println(out)
+	log.Debug("Reading connection state", "nic", nic, "out", out)
 
-	var re = regexp.MustCompile(`(?m)inet ((([0-9]{1,3}.){4})\/[0-9]{1,3}) `)
+	var re = regexp.MustCompile(`IP4\.ADDRESS\[1\]:\s+(.*)`)
 	match := re.FindAllStringSubmatch(out, -1)
 
 	if match[0] == nil {
@@ -57,25 +57,15 @@ func GetConnection(grep string) (*Connection, error) {
 	}
 	ip := match[0][1]
 
-	out, err = utils.ExecSh(fmt.Sprintf("ip -o -f inet route | grep default | grep %s", nic))
-	if err != nil {
-		return nil, fmt.Errorf("reading network route: %w", err)
-	}
-
-	re = regexp.MustCompile(`(?m)(via (([0-9]{1,3}.){4}))`)
+	re = regexp.MustCompile(`IP4\.GATEWAY:\s+(.*)`)
 	match = re.FindAllStringSubmatch(out, -1)
 
 	if match[0] == nil {
 		return nil, fmt.Errorf("reading network link: no match: output: %s", out)
 	}
-	gateway := match[0][2]
+	gateway := match[0][1]
 
-	out, err = utils.ExecSh(fmt.Sprintf("ip -o -f inet link | grep %s", nic))
-	if err != nil {
-		return nil, fmt.Errorf("reading network link: %w", err)
-	}
-
-	re = regexp.MustCompile(`(?m)link\/ether (([a-f0-9]{2}\:?){5}[a-f0-9]{2})`)
+	re = regexp.MustCompile(`GENERAL\.HWADDR:\s+(.*)`)
 	match = re.FindAllStringSubmatch(out, -1)
 
 	if match[0] == nil {
