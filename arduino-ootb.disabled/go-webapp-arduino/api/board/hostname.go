d1ad8931bd722cc502d3aacd2071f7719aefe349
Massimo Pennazio <maxipenna@libero.it>
2022-03-10 11:03:57 +0100

    Fixed avahi hostname reading

--- arduino-ootb.disabled/go-webapp-arduino/api/board/hostname.go
+++ arduino-ootb.disabled/go-webapp-arduino/api/board/hostname.go
@@ -7,17 +7,19 @@ import (
 )
 
 func ReadHostname() (string, error) {
-	// @TODO: we need to know our ip address for this command to work
-	// out, err := utils.ExecSh("avahi-resolve -a 192.168.1.198 | cut -f 2")
-	out, err := utils.ExecSh("hostname")
+	// @TODO: here we can use usb0 ip which is known, however
+	// this imply board is connected through usb to user's Pc
+	out, err := utils.ExecSh("avahi-resolve -a 192.168.7.1 | cut -f 2 | awk -F "." '{print $1}'")
 	if err != nil {
-		return "", fmt.Errorf("getting avahi hostname: %w", err)
+		return "", fmt.Errorf("reading avahi hostname: %w", err)
 	}
 
 	return strings.Replace(out, "\n", "", 1), err
 }
 
 func UpdateHostname(hostname string) (string, error) {
+	// @TODO: this is not persistent accross daemon restarts. To make it persistent
+	// need to add host-name= to /etc/avahi/avahi-daemon.conf
 	_, err := utils.ExecSh(fmt.Sprintf("avahi-set-host-name %s", hostname))
 	if err != nil {
 		return "", fmt.Errorf("updating avahi hostname: %w", err)
