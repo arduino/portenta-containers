0664144dd9daca4dfa4dc3fe55ea15d926d006dc
Massimo Pennazio <maxipenna@libero.it>
2022-03-10 09:52:52 +0100

    We need to deal with avahi hostname

--- arduino-ootb.disabled/go-webapp-arduino/api/board/hostname.go
+++ arduino-ootb.disabled/go-webapp-arduino/api/board/hostname.go
@@ -7,18 +7,20 @@ import (
 )
 
 func ReadHostname() (string, error) {
+	// @TODO: we need to know our ip address for this command to work
+	// out, err := utils.ExecSh("avahi-resolve -a 192.168.1.198 | cut -f 2")
 	out, err := utils.ExecSh("hostname")
 	if err != nil {
-		return "", fmt.Errorf("reading Factory name: %w", err)
+		return "", fmt.Errorf("getting avahi hostname: %w", err)
 	}
 
 	return strings.Replace(out, "\n", "", 1), err
 }
 
 func UpdateHostname(hostname string) (string, error) {
-	_, err := utils.ExecSh(fmt.Sprintf("hostname %s", hostname))
+	_, err := utils.ExecSh(fmt.Sprintf("avahi-set-host-name %s", hostname))
 	if err != nil {
-		return "", fmt.Errorf("reading Factory name: %w", err)
+		return "", fmt.Errorf("updating avahi hostname: %w", err)
 	}
 
 	return ReadHostname()
