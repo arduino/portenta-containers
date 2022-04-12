8b824a2f07fa5a5a1761c0811a29c063acae09a4
Massimo Pennazio <maxipenna@libero.it>
2022-04-12 18:13:10 +0200

    Picked relevant changes from devel branch
    5799534 Removed env_file still present in yml
    edfad13 We import the hw info env variables in entrypoint.sh
    7861e17 This fix container env since these variables are not defined during init
    125d699 Since pylibiio doesn't work, drop it
    
    Signed-off-by: Massimo Pennazio <maxipenna@libero.it>

--- go-webapp-arduino/entrypoint.sh
+++ go-webapp-arduino/entrypoint.sh
@@ -7,6 +7,9 @@ chmod 700 /root/.ssh
 chmod 644 /root/.ssh/id_rsa.pub
 chmod 600 /root/.ssh/id_rsa
 
+echo "Importing hw info env"
+export $(grep -v '^#' /run/arduino_hw_info.env | xargs)
+
 echo "Starting go application"
 cd /app
 ./x8-ootb
