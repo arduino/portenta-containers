fed7f44c3e21de6e3517770354c788ee1f8ba5a9
Massimo Pennazio <maxipenna@libero.it>
2023-01-13 11:45:19 +0100

    Updated debian-base container builded offline

--- debian-base-imx8-aarch64.disabled/imx8-libs.sh
+++ debian-base-imx8-aarch64.disabled/imx8-libs.sh
@@ -3,9 +3,9 @@ set -ex
 
 #Installing libdrm-imx
 cd /root/
-git clone -b libdrm-imx-2.4.109 http://source.codeaurora.org/external/imx/libdrm-imx.git
+git clone -b libdrm-imx-2.4.102 http://source.codeaurora.org/external/imx/libdrm-imx.git
 cd /root/libdrm-imx/
-git checkout 1d943abb1fccc350a129950d2690948f64503bb7
+git checkout 93c9a82bdf31ea055d45cb54ce57cd42e22c90ae
 meson --prefix /usr --buildtype plain -Damdgpu=true -Dcairo-tests=false -Detnaviv=true -Dexynos=false -Dfreedreno=true -Dfreedreno-kgsl=false -Dinstall-test-programs=true -Dintel=true -Dlibkms=true -Dman-pages=false -Dnouveau=true -Domap=true -Dradeon=true -Dtegra=false -Dudev=false -Dvalgrind=false -Dvc4=true -Dvivante=true -Dvmwgfx=true build/
 ninja -C build/ install
 
@@ -26,5 +26,6 @@ cp -ra /root/imx-gpu-g2d-6.4.3.p2.4-aarch64/g2d/usr/lib/* /usr/lib/aarch64-linux
 
 #Removing files
 rm -rf /root/libdrm-imx*
+rm -rf /root/0001-meson-add-libdrm-vivante-to-the-meson-meta-data.patch
 rm -rf /root/imx-gpu-g2d-6.4.3.p2.4-aarch64*
 rm -rf /root/imx-gpu-viv-6.4.3.p2.4-aarch64*
