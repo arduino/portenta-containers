a7c68fdea959098cb8863d1e7b485e16969e84e6
Luca Barbetti <lbarbetti@live.it>
2024-01-30 10:41:04 +0100

    Remove hardcoded IPs from vite config

--- arduino-ootb-webapp/webapp/vite.config.ts
+++ arduino-ootb-webapp/webapp/vite.config.ts
@@ -1,20 +1,24 @@
 import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from "vite";
 import eslintPlugin from "vite-plugin-eslint";
 
-export default {
-  plugins: [eslintPlugin({}), react()],
-  server: {
-    port: 3002,
-    proxy: {
-      // "/api": "http://127.0.0.1:1323",
-      // '/api': 'http://192.168.8.1' // Board IP
-      // '/api': 'http://192.168.178.98:1323' // External IP
-      "/api": "http://10.0.0.21",
-      "/api/shell": {
-        target: "http://10.0.0.21",
-        changeOrigin: true,
-        ws: true,
+
+// https://vitejs.dev/config/
+export default defineConfig((config) => {
+  const env = loadEnv(config?.mode, process.cwd(), "VITE");
+
+  return {
+    plugins: [eslintPlugin({}), react()],
+    server: {
+      port: 3002,
+      proxy: {
+        "/api": env.VITE_API_PROXY_TARGET,
+        "/api/shell": {
+          target: env.VITE_API_PROXY_TARGET,
+          changeOrigin: true,
+          ws: true,
+        },
       },
     },
-  },
-};
+  };
+});
