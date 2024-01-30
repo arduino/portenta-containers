67df9624ca39add591cf9086d938f97033f0e923
Luca Barbetti <lbarbetti@live.it>
2024-01-30 10:41:04 +0100

    Remove duplicate vite config

--- arduino-ootb-webapp/webapp/vite.config.ts
+++ arduino-ootb-webapp/webapp/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import eslintPlugin from "vite-plugin-eslint";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+export default {
+  plugins: [eslintPlugin({}), react()],
+  server: {
+    port: 3002,
+    proxy: {
+      "/api": "http://127.0.0.1:1323",
+      // '/api': 'http://192.168.8.1' // Board IP
+      // '/api': 'http://192.168.178.98:1323' // External IP
+    },
+  },
+};
