3510a4990890621d3e6d12d5685a0f4278239cd1
Massimo Pennazio <maxipenna@libero.it>
2024-09-26 17:12:08 +0200

    Code cleanup

--- python-rpc-sensors/python/main.py
+++ python-rpc-sensors/python/main.py
@@ -2,14 +2,13 @@ import time
 from msgpackrpc import Address as RpcAddress, Client as RpcClient, error as RpcError
 
 # Fixed configuration parameters
-port = 8884
 publish_interval = 5
 
 # The M4 Proxy address needs to be mapped via Docker's extra hosts
-m4_proxy_address = 'm4-proxy'
-m4_proxy_port = 5005
+m4_proxy_host = 'm4-proxy'
+m4_proxy_call_port = 5001
 
-def get_data_from_m4():
+def get_data_from_m4(rpc_client):
     """Get data from the M4 via RPC (MessagePack-RPC)
 
     The Arduino sketch on the M4 must implement the following methods
@@ -22,28 +21,16 @@ def get_data_from_m4():
     * `altitude`
 
     """
-    rpc_address = RpcAddress(m4_proxy_address, m4_proxy_port)
     data = ()
     try:
-        rpc_client = RpcClient(rpc_address)
         temperature = rpc_client.call('temperature')
-
-        rpc_client = RpcClient(rpc_address)
         humidity = rpc_client.call('humidity')
-
-        rpc_client = RpcClient(rpc_address)
         pressure = rpc_client.call('pressure')
-
-        rpc_client = RpcClient(rpc_address)
         gas = rpc_client.call('gas')
-
-        rpc_client = RpcClient(rpc_address)
         altitude = rpc_client.call('altitude')
-
         data = temperature, humidity, pressure, gas, altitude
     except RpcError.TimeoutError:
         print("Unable to retrive data from the M4.")
-
     return data
 
 if __name__ == '__main__':
@@ -55,8 +42,10 @@ if __name__ == '__main__':
     print()
 
     try:
+        rpc_address = RpcAddress(m4_proxy_host, m4_proxy_call_port)
+        rpc_client = RpcClient(rpc_address)
         while True:
-            data = get_data_from_m4()
+            data = get_data_from_m4(rpc_client)
             if len(data) > 0:
                 print("Temperature: ", data[0])
                 print("Humidity: ", data[1])
@@ -64,5 +53,5 @@ if __name__ == '__main__':
                 print("Gas: ", data[3])
                 print("Altitude: ", data[4])
             time.sleep(publish_interval)
-    except KeyboardInterrupt:
-        print('Stopped.')
+    except Exception as e:
+        print(f"Failed to create RpcClient: {e}")
