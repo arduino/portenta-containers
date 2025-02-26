04dc683ce9e0b9a77f3b4d27680ee3904c91f3d1
mat83 <mattia.scattolin@icloud.com>
2025-02-26 16:56:44 +0100

    ide 2 container

---
+++ arduino-ide-2/start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+# wait for display
+if [ ! -e /sys/class/graphics/fb0 ]; then
+  echo "Display not detected.  Will wait for it to show up ..."
+fi
+while [ ! -e /sys/class/graphics/fb0 ]; do
+  sleep 1
+done
+
+# use /tmp for config and cache data in read-only mode
+mkdir -p ${XDG_CACHE_HOME} ${XDG_CONFIG_HOME}
+
+./arduino-ide $@
+
